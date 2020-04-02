9f835d9cc29123ae77efce570f14de90350d7e3d
Zachary Lawton <22178961+ApacheHelicopter@users.noreply.github.com>
2020-04-01 22:45:50 -0400

    Add files via upload

---
+++ ChickenGun.cs
@@ -0,0 +1,41 @@
+﻿using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace e.Items.Guns
+{
+	public class ChickenGun : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("The Cock");
+			Tooltip.SetDefault("Eggcelent Gunmanship");
+		}
+		public override void SetDefaults()
+		{
+			item.damage = 20;
+			item.ranged = true;
+			item.width = 40;
+			item.height = 20;
+			item.useTime = 20;
+			item.useAnimation = 20;
+			item.useStyle = 5;
+			item.noMelee = true; //so the item's animation doesn't do damage
+			item.knockBack = 4;
+			item.value = 10000;
+			item.rare = 2;
+			item.UseSound = SoundID.Item11;
+			item.autoReuse = true;
+			item.shoot = 10; //idk why but all the guns in the vanilla source have this
+			item.shootSpeed = 16f;
+			item.useAmmo = AmmoID.Bullet;
+		}
+		public override void AddRecipes()
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ItemID.Wood, 10);
+			recipe.AddTile(TileID.WorkBenches);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+		}
+	}
+}
---
+++ ExampleBullet.cs
@@ -0,0 +1,45 @@
+
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+namespace e.Items.Guns
+{
+	public class ExampleBullet : ModItem
+	{
+		public override void SetStaticDefaults() {
+			Tooltip.SetDefault("This is a modded bullet ammo.");
+		}
+
+		public override void SetDefaults() {
+			item.damage = 10;
+			item.ranged = true;
+			item.width = 8;
+			item.height = 8;
+			item.maxStack = 999;
+			item.consumable = true;             //You need to set the item consumable so that the ammo would automatically consumed
+			item.knockBack = 40.0f;
+			item.value = 10;
+			item.rare = 4;
+			item.shoot = ProjectileType<Projectiles.SadBird>();   //The projectile shoot when your weapon using this ammo
+			item.shootSpeed = 0.1f;                  //The speed of the projectile
+			item.ammo = AmmoID.Bullet;              //The ammo class this ammo belongs to.
+		}
+
+		// Give each bullet consumed a 20% chance of granting the Wrath buff for 5 seconds
+		public override void OnConsumeAmmo(Player player) {
+			if (Main.rand.NextBool(2)) {
+				player.AddBuff(BuffID.Honey, 500);
+			}
+		}
+
+		public override void AddRecipes() {
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ItemID.DirtBlock, 1);
+			recipe.AddTile(TileID.WorkBenches);
+			recipe.SetResult(this, 100);
+			recipe.AddRecipe();
+		}
+	}
+}
