b4afdcac08e140e5fcfb8396f34a8bdbaf746cfa
Zachary Lawton <22178961+ApacheHelicopter@users.noreply.github.com>
2020-04-02 00:05:11 -0400

    Create ChickenGun2.cs

---
+++ Items/Weapons/Guns/ChickenGun2.cs
@@ -0,0 +1,42 @@
+﻿using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Modding.Items.Weapons.Guns
+{
+	public class ChickenGun2: ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("The Cockernator");
+			Tooltip.SetDefault("Eggcelent Destruction");
+		}
+		public override void SetDefaults()
+		{
+			item.damage = ChickenGun.item.damage * 2;
+			item.ranged = true;
+			item.width = 40;
+			item.height = 20;
+			item.useTime = 20;
+			item.useAnimation = 20;
+			item.useStyle = 5;
+			item.noMelee = true; //so the item's animation doesn't do damage
+			item.knockBack = 4;
+			item.value = 10000;
+			item.rare = 2;
+			item.UseSound = SoundID.Item11;
+			item.autoReuse = true;
+			item.shoot = 10; //idk why but all the guns in the vanilla source have this
+			item.shootSpeed = 16f;
+			item.useAmmo = AmmoID.Bullet;
+		}
+		public override void AddRecipes()
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ItemID.Wood, 10);
+			recipe.AddIngredient(ItemID.ChickenGun, 1);
+			recipe.AddTile(TileID.WorkBenches);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+		}
+	}
+}
